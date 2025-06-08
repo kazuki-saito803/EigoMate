fef4ee3cdc0865cb8d0d2d5d575a4b42686b7973
kaz.saito <kaz.saito803@gmail.com>
2025-06-08 19:37:36 +0900

    initial commit

---
+++ eigomatefunc/function_app.py
@@ -0,0 +1,34 @@
+import os
+import logging
+import azure.functions as func
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route(route="http_trigger_line")
+def http_trigger_line(req: func.HttpRequest) -> func.HttpResponse:
+    signature = req.headers.get('X-Line-Signature')
+    body = req.get_body().decode('utf-8')
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        logging.error("Invalid signature")
+        return func.HttpResponse("Invalid signature", status_code=400)
+
+    return func.HttpResponse('OK', status_code=200)
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='Hello world')
+    )
